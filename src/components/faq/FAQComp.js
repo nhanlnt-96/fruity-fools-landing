0e99a63a34f5632783787cda19f690dcac3ca4c2
nhanlnt-96 <nhanlnt@hotmail.com>
2022-01-14 01:54:59 +0700

    [faq]: Complete create FAQ component

---
+++ src/components/faq/FAQComp.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {Accordion, Container} from "react-bootstrap";
+import {faqData} from "../../configs/faqData";
+
+import './FAQComp.scss';
+
+const FaqComp = () => {
+  return (
+    <Container fluid className="faq-comp comp-height">
+      <Container className="faq-comp-container d-flex flex-column justify-content-center align-items-center">
+        <h6 data-aos="fade-up" className="title">FAQ</h6>
+        <Accordion className="accordion-custom">
+          {
+            faqData.map((main, index) => (
+              <Accordion.Item data-aos={!(index === faqData.length - 1) ? "fade-up" : null} key={index}
+                              eventKey={index.toString()}>
+                <Accordion.Header>{main.question}</Accordion.Header>
+                <Accordion.Body>
+                  {
+                    main.answer.map((item, key) => (
+                      <p key={key} className="answer-content">{item}</p>
+                    ))
+                  }
+                </Accordion.Body>
+              </Accordion.Item>
+            ))
+          }
+        </Accordion>
+      </Container>
+    </Container>
+  );
+};
+
+export default FaqComp;
---
+++ src/configs/faqData.js
@@ -0,0 +1,31 @@
+export const faqData = [
+  {
+    question: "How Many Fruity Fools will be available on the first launch?",
+    answer: ["We have decided to come up with a collection of 10,000."]
+  },
+  {
+    question: "How much will it cost to mint a Fruity Fool?",
+    answer: ["47 Matic to 94 Matic varying range based on rarity availability"]
+  },
+  {
+    question: "How Many Fruity Fools can I mint?",
+    answer: ["As many as you like, we understand its hard to avoid making Foolish decisions when it comes to our" +
+    " Fruity Fools launch."]
+  },
+  {
+    question: "Where Can I mint a Fruity Fool?",
+    answer: ["You can simply mint on our website."]
+  },
+  {
+    question: "What chain is Fruity Fools hosted on?",
+    answer: ["Fruity Fools will be on the Polygon Blockchain."]
+  },
+  {
+    question: "Are Fruity Fools hand drawn or generated?",
+    answer: ["Fruity Fools has gotten their inspiration from a member of the team drawing original and painting on a Canvas the very first take on a Fruity Fools concept. The 10,000 pieces have been carefully designed, developed and generated. With their own uniqueness."]
+  },
+  {
+    question: "When is the release date?",
+    answer: ["Coming Soon"]
+  },
+]
